06548aa645214f16631183c826ec945ba7e7e88b
Dom Kiva-Meyer <git@domkm.com>
2012-09-22 15:29:31 -0700

    adds company model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120922222726) do
+ActiveRecord::Schema.define(:version => 20120922222857) do
+
+  create_table "companies", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "employees", :force => true do |t|
     t.integer  "user_id"
